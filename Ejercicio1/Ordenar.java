37f167d05d5bf35313abbcf58a153c43a7b2a412
Mar <66106505+Marvoo200@users.noreply.github.com>
2022-11-17 14:09:27 -0600

    Update Ordenar.java

--- Ejercicio1/Ordenar.java
+++ Ejercicio1/Ordenar.java
@@ -1,31 +1,69 @@
 package Ejercicio1;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 
 public class Ordenar {
 
-    public double[] ordenar(String numero[]) throws Excepciones{
-        Scanner leer = new Scanner(System.in);
-        double a[] = {0, 0, 0, 0};
+    public int[] ordenar(String numero1, String numero2, String numero3, String numero4) throws Exception{
+        
+        int n[] ={0, 0, 0, 0};
+        int g[] = {10, 10, 10, 10 };
+        int f = 0;
+        
+        String a[] ={numero1, numero2, numero3, numero4};
+        
+            try {
+                
+                
+                for(int i=0; i<4; i++){
+                    if(Integer.parseInt(a[i]) != 0){
+                        n[f] =Integer.parseInt(a[i]);
+                        f++;
+                    }else{
+                        g[i] = 1;
+                    }
+                }
+                Arrays.sort(n);
+    
+            }  catch (NumberFormatException e) {
+    
+                System.out.println("\nUno de los números ingresados no es valido.\n");
+    
+            }
+            finally{
+                // f = 0;
+                // for(int i=0; i<4; i++){
+                    
+                //     if(g[i] != 1){
+                //         n[f] =Integer.parseInt(a[i]);
+                //         f++;
+                //     }else{
 
-        for(int i=0; i<4; i++){
-           
-            
-            a[i] = Double.parseDouble(numero[i]);
-            
-            // if(a[i] == 0){
-            //     throw new Excepciones("Se debe de ingresar un número");
-            // }
-            
+                //     }
+                    
+                // }
+                Arrays.sort(n);
+                
+                System.out.println("Los numeros ordenados son: ");
+                for(int i=0; i< f; i++){
+                    
+                    System.out.print(" "+n[i]);
+                   
+                       
+                }
+            }
+        
+        
 
-        }
-        Arrays.sort(a);       
+        
+             
     
                    
-        leer.close();
-        return a;
+        return n;
     }
+    
 }
+
+
 
