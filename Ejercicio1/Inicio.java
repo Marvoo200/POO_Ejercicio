70759ce345257b7306d3336bcc54cf89d40f9f27
Mar <66106505+Marvoo200@users.noreply.github.com>
2022-11-17 19:09:25 -0600

    Se modificó el programa para su correcto funcionamiento

--- Ejercicio1/Inicio.java
+++ Ejercicio1/Inicio.java
@@ -7,23 +7,13 @@
 //Este es el link de github: https://github.com/Marvoo200/POO_Ejercicio.git
 package Ejercicio1;
 
-
 public class Inicio {
 
     public static void main(String[] args) throws Excepcion {
         
-        int numeros[] = new int[5];
-        
-
+        double numeros[] = {0, 0, 0, 0};
         Ordenar num = new Ordenar();
         numeros = num.ingresar_numeros(); //ingresamos numeros
-        num.ordenar(numeros); //ordenamos los numeros
-
-        
-        
-
-
-            
+        num.ordenar(numeros);
     }
 }
-
